36da004fb01f8e0127c1c32f6cd9ed36c72af555
Dhanashree <dhanashreekbanshinge@gmail.com>
2024-12-08 23:04:49 +0530

    updated final instance

--- Jenkins/jenkins_advance/ec2_instance/main.tf
+++ Jenkins/jenkins_advance/ec2_instance/main.tf
@@ -17,6 +17,7 @@ resource "aws_instance" "server" {
 
    }
 
+
 }
 
 resource "aws_security_group" "sg" {
@@ -38,6 +39,15 @@ resource "aws_vpc_security_group_ingress_rule" "for_ssh" {
   ip_protocol       = "tcp"
   to_port           = 22
 }
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
+  security_group_id = aws_security_group.sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+
+
 
 
 
